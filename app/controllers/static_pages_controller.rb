d5703ea387378f40b5bfff90c22377c55bb83463
hisrael1 <harrisonisrael23@gmail.com>
2022-04-27 10:21:38 -0400

    root is up

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+# Did I create the proj with --database-postgresql + the other command?
+# Is my controller named correctly?
+class StaticPagesController < ApplicationController
+    # did I define the root correctly?
+    def root
+    end
+end

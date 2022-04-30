9ed25d30163c136ab9e4043a1237a5079b263338
hisrael1 <harrisonisrael23@gmail.com>
2022-04-30 18:09:39 -0400

    first commit

---
+++ frontend/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GreetingContainer from "./greeting/greeting_container";
+
+const App = () => (
+    <div>
+        <GreetingContainer/>
+    </div>
+);
+
+export default App;
--- frontend/land_bnb.jsx
+++ frontend/land_bnb.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Root from "./components/root";
+import configureStore from './store/store';
 
+
+/// I'm gettting an error because my package json is incomplete
+// I need to reinstall my package json. Might wanna push to github first.
 document.addEventListener("DOMContentLoaded", () => {
     const store = configureStore();
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
+    // for testing purposes
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
     // window.login = login;
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Congrats, you broke React!</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });

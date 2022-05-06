e93f9863cfd06a18e9f7c1844956b7fff1abf1f3
hisrael1 <harrisonisrael23@gmail.com>
2022-05-06 10:25:10 -0400

    splash

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -10,16 +10,12 @@ import Splash from "./splash/splash";
 // I think I need to set up my router to only show all of the components I'm about to create if I'm logged in, by using routes.
 // I think I need to create bunch of components and then style those components.
 const App = () => (
-    <div id="container">
-        {/* I think I can delete this <GreetingContainer /> */}
-
-        <Switch>
-            {/* Just changed from reg route. Anything else I need to do? */}
-            <AuthRoute path="/login" component={LoginFormContainer} />
-            <AuthRoute path="/signup" component={SignupFormContainer} />
-            <Route path = "/" component={Splash} />
-        </Switch>
-    </div>
+    <Switch>
+        {/* Just changed from reg route. Anything else I need to do? */}
+        <AuthRoute path="/login" component={LoginFormContainer} />
+        <AuthRoute path="/signup" component={SignupFormContainer} />
+        <Route path = "/" component={Splash} />
+    </Switch>
 );
 
 export default App;
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -7,23 +7,89 @@ class Splash extends React.Component {
 
         return(
             <div>
-                <div id="header">
-                    <p id='name'>
-                        landbnb
-                    </p>
+                <div id="container">
+                    <div id="header">
+                        <p id='name'>
+                            landbnb
+                        </p>
 
-                    <p id="search">
-                        <input type="text" id="left-edge-search"/>
-                        <input type="date" id="left-mid-search"/>
-                        <input type="date" id="right-mid-search"/>
-                        <input type="int" id="right-edge-search"/>
-                    </p>
+                        <p id="search">
+                            <input type="text" id="left-edge-search"/>
+                            <input type="date" id="left-mid-search"/>
+                            <input type="date" id="right-mid-search"/>
+                            <input type="int" id="right-edge-search"/>
+                        </p>
 
-                    <Menu />
+                        <Menu />
+                    </div>
+
+                    <div id='splash-main-pic-container'>
+                        <img src={window.splash_main_pic} id="splash-main-pic"/>
+                    </div>
                 </div>
 
-                <div id='splash-main-pic-container'>
-                    <img src={window.splash_main_pic} id="splash-main-pic"/>
+                <div>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+
+                        <p id="inspiration">
+                            Inspiration for your next trip
+                        </p>
+
+                        <br />
+                        <br />
+                        <br />
+
+                        <div id="city-container" class="cities">
+                            <div id='montauk' class="city">
+                                <img src={window.montauk} id="montauk"/>
+                                <p class="cty-text">Montauk</p>
+                            </div>
+                            <div id='lakegeorge' class="city">
+                                <img src={window.lakegeorge} id="lakegeorge"/>
+                                <p class="cty-text">Lake George</p>
+                            </div>
+                            <div id='newport' class="city">
+                                <img src={window.newport} id="newport"/>
+                                <p class="cty-text">Newport</p>
+                            </div>
+                            <div id='scranton' class="city">
+                                <img src={window.scranton} id="scranton"/>
+                                <p class="cty-text">
+                                    Scranton
+                                </p>
+                            </div>
+                        </div>
+
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+
+
+                        <div id="footer">
+                            <div>
+                                <p class="bold-footer">
+                                    About
+                                </p>
+                            </div>
+
+                            <div>
+                                <p class="bold-footer">
+                                    Resources
+                                </p>
+                            </div>
+                        </div>
                 </div>
             </div>
         )
